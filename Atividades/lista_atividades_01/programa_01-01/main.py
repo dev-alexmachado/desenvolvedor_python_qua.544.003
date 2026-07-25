4388143d0aadee74749396dba49df16e17a22aa2
dev-alexmachado <alexmachado.ribeiro@gmail.com>
2026-07-24 21:32:07 -0300

    Aula do dia 24/07/2026.

---
+++ Atividades/lista_atividades_01/programa_01-01/main.py
@@ -0,0 +1,5 @@
+# TODO: atividade 01
+"""
+Crie um programa que receba o nome, peso e altura do usuário, e informe na tela o seu IMC o seu diagnóstico com base no valor do IMC.
+"""
+# NOTE: imc = peso/(altura**2)
