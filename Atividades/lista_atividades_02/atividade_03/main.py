637db6312b2452f3f5eea31198ba902480710aa2
dev-alexmachado <alexmachado.ribeiro@gmail.com>
2026-08-11 19:46:08 -0300

    Aula do dia 11/08/2026.

---
+++ Atividades/lista_atividades_02/atividade_03/main.py
@@ -0,0 +1,8 @@
+# TODO: atividade 03
+# Crie um programa que receba o nome de um aluno e 3 notas.
+# O programa deve calcular a média do aluno e informar se
+# o aluno está aprovado (média mínima = 7) ou reprovado.
+# O programa deve gravar esses dados em um JSON.
+# Ao final, o usuário deverá escolher se deseja inserir as
+# notas de outro aluno, que deverão ser gravadas no mesmo
+# arquivo JSON.
