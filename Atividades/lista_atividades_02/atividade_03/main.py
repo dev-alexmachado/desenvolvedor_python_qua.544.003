46af44075f703d446172a0af5b61c07adaff69d1
dev-alexmachado <alexmachado.ribeiro@gmail.com>
2026-08-12 21:35:34 -0300

    Aula do dia 12/08/2026.

--- Atividades/lista_atividades_02/atividade_03/main.py
+++ Atividades/lista_atividades_02/atividade_03/main.py
@@ -1,3 +1,38 @@
+import os
+import json
+
+alunos = []
+
+os.system("cls" if os.name == "nt" else "clear")
+
+while True:
+    print("1 - Informa dados")
+    print("2 - Sair do programa")
+    opcao = input("Informe a opção: ").strip()
+    os.system("cls" if os.name == "nt" else "clear")
+    match opcao:
+        case "1":
+            aluno = {}
+            notas = [0,0,0]
+            aluno['nome'] = input("Informe o nome do aluno: ").strip().title()
+            for i in range(len(notas)):
+                notas[i] = float(input(f"Informe a {i+1}ª nota: ").replace(",","."))
+            aluno['notas'] = notas
+            aluno['média'] = sum(notas)/len(notas)
+            aluno['resultado'] = "aprovado" if aluno['média'] >= 7 else "reprovado"
+            alunos.append(aluno)
+
+            # FIXME: verifique o encoding
+            with open("atividade_03/arquivo.json","w",encoding="utf-8") as f:
+                json.dump(alunos, f)
+            print("Dados do aluno gravados com sucesso!")
+            continue
+        case "2":
+            break
+        case _:
+            print("Opção inválida.")
+            continue
+
 # TODO: atividade 03
 # Crie um programa que receba o nome de um aluno e 3 notas.
 # O programa deve calcular a média do aluno e informar se
---
+++ Parte03-Funcoes/01_funcao/main.py
@@ -0,0 +1,6 @@
+# declara uma função
+def boas_vindas():
+    print("Seja bem vindo! 😎🐍")
+
+# algoritmo principal
+boas_vindas()
